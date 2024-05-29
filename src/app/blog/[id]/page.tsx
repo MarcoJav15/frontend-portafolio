cbf5ba84239c291063f26800f9cf233854462e9c
MarcoJav15 <marcodeleon2002@gmail.com>
2024-05-29 11:05:15 -0600

    Ajustando el BlogCard

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -33,12 +33,12 @@ export default function UserProfile({ params }: Props) {
     }, [params.id]);
 
     return (
-        <main className="container mx-auto p-6">
-            <h1 className="text-5xl font-bold mb-8 text-center">Datos del Blog</h1>
+        <main className="container mx-auto p-6 bg-black min-h-screen">
+            <h1 className="text-5xl font-bold mb-8 text-center text-white">Datos del Blog</h1>
             {project && (   
-                <div className="bg-gray rounded-lg shadow-md p-6">
-                    <h2 className="text-3xl font-bold mb-4">{project.name}</h2>
-                    <p className="text-gray-700 text-lg text-justify">{project.description}</p>
+                <div className="bg-gray-800 border border-gray-600 rounded-lg shadow-lg p-6 hover:shadow-xl">
+                    <h2 className="text-3xl font-bold mb-4 text-yellow-400">{project.name}</h2>
+                    <p className="text-gray-300 text-lg text-justify">{project.description}</p>
                 </div>
             )}
         </main>
