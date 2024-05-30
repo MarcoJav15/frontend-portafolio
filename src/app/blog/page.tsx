d2449edcaffc6a02272fff38e2000ecd96e1bcca
MarcoJav15 <marcodeleon2002@gmail.com>
2024-05-29 19:04:06 -0600

    Cambios al blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -13,7 +13,7 @@ export default async function page() {
   const response = await axios.get<Projects[]>(`${process.env.NEXT_PUBLIC_API_URL}/projects`);
 
   return (
-    <section className="container mx-auto p-6">
+    <section className="container mx-auto p-6 bg-black min-h-screen">
       <div className="fixed top-4 left-4 z-10">
         <Link href="/">
           <button className="px-3 py-1 bg-blue-600 text-white text-sm rounded-full hover:bg-blue-700 transition-colors duration-300">
@@ -21,13 +21,12 @@ export default async function page() {
           </button>
         </Link>
       </div>
-      <h1 className="text-5xl font-bold mb-8 text-center">Blogs</h1>
+      <h1 className="text-5xl font-bold mb-8 text-center text-white">Blogs</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {response.data.map((project) => (
           <BlogCard key={project.id} id={project.id} name={project.name} description={project.description} />
         ))}
       </div>
-
     </section>
   );
 }
